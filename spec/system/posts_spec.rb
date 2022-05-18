ad6874246334fd75203ca6476451ed3c8004502e
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2022-05-18 23:36:14 +0900

    use let to test details of posts

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Manage posts function", type: :system do
+	let(:user_a) { FactoryBot.create(:user, email: "userA@gmail.com") }
+    let(:user_b) { FactoryBot.create(:user, email: "userB@gmail.com") }
+    let!(:post_a){ FactoryBot.create(:post, title: "hehe1111", user: user_a)}
+    #postA = FactoryBot.create(:post, title: "hehe1111", user: user_a)
+
+	before do
+		#driven_by(:selenium_chrome_headless)
+		driven_by(:rack_test)
+
+		# user_a = FactoryBot.create(:user)
+		FactoryBot.create(:post, title:"hello1234", user: user_a)
+		visit root_path
+		fill_in "Email", with: loginn_user.email
+		fill_in "Password", with: loginn_user.password
+		click_button "Log in"
+	end
+
+
 	describe "Show posts list" do
-		let(:user_a) { FactoryBot.create(:user, email: "userA@gmail.com", password: "khoa1998") }
-	    let(:user_b) { FactoryBot.create(:user, email: "userB@gmail.com", password: "khoa1998") }
-
-
-		before do
-			#driven_by(:selenium_chrome_headless)
-			#driven_by(:rack_test)
-
-			# user_a = FactoryBot.create(:user)
-			FactoryBot.create(:post, title:"hello1234", user: user_a)
-			visit root_path
-			fill_in "Email", with: loginn_user.email
-			fill_in "Password", with: loginn_user.password
-			click_button "Log in"
-		end
-
 		context 'When user A login ' do
 			let(:loginn_user) { user_a }
 
@@ -34,6 +36,22 @@ RSpec.describe "Manage posts function", type: :system do
 				expect(page).to have_no_content("hello1234")
 			end
 		end
+	end 	# describe "Show posts list"
 
+
+	describe "show details of post" do
+		context "When user A login" do
+			let(:loginn_user) {user_a}
+
+			before do
+				#postA = FactoryBot.create(:post, title: "hehe1111", user: user_a)
+				visit post_path(post_a)
+			end
+
+			it "Show posts of user A" do
+				expect(page).to have_content("hehe1111")
+			end
+		end
 	end
+
 end
