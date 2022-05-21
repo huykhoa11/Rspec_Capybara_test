bc873ffd813d457e58bb0fa3fa274f2e8b464aac
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2022-05-21 19:23:23 +0900

    Add cancancan + ransack

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.2'
 gem 'devise'
 gem 'cancancan'
+gem 'ransack'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.5', '>= 6.1.5.1'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,9 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    #@posts = Post.all
+    @q = current_user.posts.ransack(params[:q])
+    @posts = @q.result(distinct: true)
   end
 
   # GET /posts/1 or /posts/1.json
